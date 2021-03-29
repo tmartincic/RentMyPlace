1d0350f883f8609bd799329f820e2d506b003e46
Martincic <martincic.tomas@gmail.com>
2021-03-29 19:47:00 +0200

    documentation

--- RentMyPlaceISTE330/src/Init.java
+++ RentMyPlaceISTE330/src/Init.java
@@ -2,15 +2,11 @@ import java.util.ArrayList;
 
 public class Init {
     public static void main(String[] args){
-//        This is intended so methods can be chained as in example below of
-//        selecting name, price, location_id of properties from labin,
-//        where price is less then 500 and results ordered by ascending price and
-//
 
-        //Each query line can be comented out (EXCEPT get(), get() is manditory) and the querry will still perform,
+        //Each query line can be comented out (EXCEPT get(), get() is manditory) and the query will still perform,
         //try commenting .select() and you will get all properties instead of selected ones, if orWhere is commented
         //we won't return apartment with size exactly 55. OrderBy can be commented out and we wont get any order.
-        //All of these functions can be chained in any order, they can be chained as much as needed or as simply as
+        //All of these functions can be chained in ANY order and NONE ARE MANDITORY. It can be as simply as
         //new Property().get() to retrieve all properties from table "properties"
         ArrayList<Property> properties_from_labin = new Property()
                 .select(new String[]{"id", "description", "pricePerNight", "size", "bedrooms"})
