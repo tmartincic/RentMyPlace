ce77dd02b7567fc9a4a4cbc9ac62fdb44682f37c
tkuvek <tk9480@g.rit.edu>
2021-03-30 15:24:08 +0200

    Test commit

--- RentMyPlaceISTE330/src/Reservations.java
+++ RentMyPlaceISTE330/src/Reservations.java
@@ -1,66 +1,83 @@
 import java.sql.Date;
+import java.util.*;
 
-public class Reservations
+public class Reservations extends Model
 {
-    public int userId;
-    public int propertyId;
-    public java.sql.Date from;
-    public java.sql.Date to;
-    public double price;
+   private static String table_name = "reservations";    
 
-    public Reservations() {
-        this.userId = -1;
-        this.propertyId = -1;
-        this.from = null;
-        this.to = null;
-        this.price = -1;
-    }
+   public int userId;
+   public int propertyId;
+   public java.sql.Date from;
+   public java.sql.Date to;
+   public double price;
 
-    public Reservations(int userId, int propertyId, Date from, Date to, double price) {
-        this.userId = userId;
-        this.propertyId = propertyId;
-        this.from = from;
-        this.to = to;
-        this.price = price;
-    }
+   public Reservations() {
+      super(table_name);
+      this.userId = -1;
+      this.propertyId = -1;
+      this.from = null;
+      this.to = null;
+      this.price = -1;
+   }
 
-    public int getUserId() {
-        return userId;
-    }
+   public Reservations(int userId, int propertyId, Date from, Date to, double price) {
+      super(table_name);
+      this.userId = userId;
+      this.propertyId = propertyId;
+      this.from = from;
+      this.to = to;
+      this.price = price;
+   }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
+   public int getUserId() {
+      return userId;
+   }
 
-    public int getPropertyId() {
-        return propertyId;
-    }
+   public void setUserId(int userId) {
+      this.userId = userId;
+   }
 
-    public void setPropertyId(int propertyId) {
-        this.propertyId = propertyId;
-    }
+   public int getPropertyId() {
+      return propertyId;
+   }
 
-    public Date getFrom() {
-        return from;
-    }
+   public void setPropertyId(int propertyId) {
+      this.propertyId = propertyId;
+   }
 
-    public void setFrom(Date from) {
-        this.from = from;
-    }
+   public Date getFrom() {
+      return from;
+   }
 
-    public Date getTo() {
-        return to;
-    }
+   public void setFrom(Date from) {
+      this.from = from;
+   }
 
-    public void setTo(Date to) {
-        this.to = to;
-    }
+   public Date getTo() {
+      return to;
+   }
 
-    public double getPrice() {
-        return price;
-    }
+   public void setTo(Date to) {
+      this.to = to;
+   }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+   public double getPrice() {
+      return price;
+   }
+
+   public void setPrice(double price) {
+      this.price = price;
+   }
+
+   @Override
+   public ArrayList<Reservations> get() {
+      ArrayList<Reservations> reservations = new ArrayList<Reservations>();
+   
+      /*
+      Finish method
+       */
+   
+      return reservations;
+   }
+
 }
