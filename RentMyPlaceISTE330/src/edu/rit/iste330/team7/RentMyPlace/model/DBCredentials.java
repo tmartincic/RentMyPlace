c87cf86a30719ff911c91b837f0291586d17e8e0
Martincic <martincic.tomas@gmail.com>
2021-04-12 13:09:35 +0200

    DBCredentials

---
+++ RentMyPlaceISTE330/src/edu/rit/iste330/team7/RentMyPlace/model/DBCredentials.java
@@ -0,0 +1,29 @@
+package edu.rit.iste330.team7.RentMyPlace.model;
+
+public class DBCredentials {
+    private static final String DB_HOST = "localhost";
+    private static final String DB_PORT = "3306";
+    private static final String DB_DATABASE = "rent_my_place";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "";
+
+    public static String getDbHost() {
+        return DB_HOST;
+    }
+
+    public static String getDbPort() {
+        return DB_PORT;
+    }
+
+    public static String getDbDatabase() {
+        return DB_DATABASE;
+    }
+
+    public static String getDbUsername() {
+        return DB_USERNAME;
+    }
+
+    public static String getDbPassword() {
+        return DB_PASSWORD;
+    }
+}
