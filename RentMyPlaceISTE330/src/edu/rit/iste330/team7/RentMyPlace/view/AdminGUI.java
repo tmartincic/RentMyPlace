c279b96b872f87cea3bb6663653d726d2c10ec88
soguic <so6561@g.rit.edu>
2021-04-24 00:53:24 +0200

    add adminGUI

---
+++ RentMyPlaceISTE330/src/edu/rit/iste330/team7/RentMyPlace/view/AdminGUI.java
@@ -0,0 +1,120 @@
+package edu.rit.iste330.team7.RentMyPlace.view;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class AdminGUI extends JFrame{
+    private JList jListUsersInfo;
+    private JPanel main;
+    private JPanel usersSection;
+    private JPanel buttonsPanel;
+    private JButton buttonPromote;
+    private JLabel title;
+
+    public JList getjListUsersInfo() {
+        return jListUsersInfo;
+    }
+
+    public String[] getUsersList() {
+        return usersList;
+    }
+
+    public JButton getButtonRemove() {
+        return buttonRemove;
+    }
+
+    public JButton getButtonPromote() {
+        return buttonPromote;
+    }
+
+    private String[] usersList;
+    private JButton buttonRemove;
+
+    public AdminGUI() {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        initComponents();
+
+    }
+
+    private void initComponents(){
+        main = new JPanel(new GridLayout(0,1));
+
+        usersSection = new JPanel(new GridLayout(0,1));
+
+        title = new JLabel("", JLabel.CENTER);
+        title.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+
+        buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        buttonRemove = new JButton("Remove user");
+        buttonPromote = new JButton("Promote to admin");
+        buttonRemove.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        jListUsersInfo = new JList();
+        jListUsersInfo.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        usersSection.add(jListUsersInfo);
+
+        buttonsPanel.add(buttonRemove);
+        buttonsPanel.add(buttonPromote);
+
+        main.add(title);
+        main.add(usersSection);
+        main.add(buttonsPanel);
+
+
+        add(main);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+        setSize(new java.awt.Dimension(535, 343));
+        setResizable(true);
+
+    }
+
+    public String[] addUsers(ArrayList<String> users){
+        usersList = new String[users.size()];
+
+        for (int i = 0; i < users.size(); i++) {
+            usersList[i] = users.get(i);
+        }
+
+
+        jListUsersInfo.setListData(users.toArray());
+
+        return usersList;
+    }
+
+    public void setName(String name){
+        String result = "Welcome admin: " + name;
+        title.setText(result);
+    }
+
+    public void addRemoveUserListener(ActionListener ae){
+        this.getButtonRemove().addActionListener(ae);
+    }
+
+    public void addPromoteUserListener(ActionListener ae){
+        this.getButtonPromote().addActionListener(ae);
+    }
+
+
+
+}
